95bf8e5d5ce36ddcbf4658a42d9eff6aa79a224a
ushan senarathna <ushansenarathna000@gmail.com>
2025-03-12 23:55:07 +0530

    feat(spend-groups): add group list view

--- src/app/(with_nav)/spend-groups/Components/GroupList.tsx
+++ src/app/(with_nav)/spend-groups/Components/GroupList.tsx
@@ -1,4 +1,6 @@
 import DateTimeComponent from '@/components/DateTimeComponent';
+import { Divider } from '@heroui/divider';
+import Link from 'next/link';
 import React from 'react';
 
 interface GroupList {
@@ -39,6 +41,46 @@ const data: GroupList[] = [
     name: 'Starbucks',
     groupTotalSpend: 0,
   },
+  {
+    id: '5',
+    name: 'FoodCity',
+    groupTotalSpend: 0,
+  },
+  {
+    id: '6',
+    name: 'University',
+    groupTotalSpend: 0,
+  },
+  {
+    id: '7',
+    name: 'Starbucks',
+    groupTotalSpend: 0,
+  },
+  {
+    id: '8',
+    name: 'Lunch',
+    groupTotalSpend: 0,
+  },
+  {
+    id: '9',
+    name: 'Starbucks',
+    groupTotalSpend: 0,
+  },
+  {
+    id: '10',
+    name: 'Starbucks',
+    groupTotalSpend: 0,
+  },
+  {
+    id: '11',
+    name: 'Starbucks',
+    groupTotalSpend: 0,
+  },
+  {
+    id: '12',
+    name: 'Starbucks',
+    groupTotalSpend: 0,
+  },
 ];
 
 export default function GroupList() {
@@ -46,37 +88,40 @@ export default function GroupList() {
     <article className='flow-root w-full px-4'>
       <ul role='list' className='divide-y divide-gray-200 dark:divide-gray-700'>
         {data.map((group) => (
-          <li key={group.id} className='py-4'>
-            <div className='flex items-center justify-between'>
-              <div className='flex flex-col'>
-                <h3 className='text-sm font-semibold text-foreground'>
-                  {group.name}
-                </h3>
-                {group.groupTotalSpend && group.groupTotalSpend > 0 ? (
-                  <p className='text-xs font-light text-foreground/80'>
-                    {`Total spends: Rs.${group.groupTotalSpend}`}
-                  </p>
-                ) : (
-                  <p className='text-xs font-light text-gray-500'>
-                    No transactions yet
-                  </p>
-                )}
-              </div>
-              <div className='flex flex-col items-end'>
-                <p className='text-sm font-light text-foreground/80'>
-                  {group.LastTransaction && (
-                    <DateTimeComponent
-                      dateString={group.LastTransaction?.date}
-                      type='default'
-                    />
+          <Link key={group.id} href={`spend-groups/my/${group.id}`}>
+            <li key={group.id} className='py-3'>
+              <div className='flex items-center justify-between'>
+                <div className='flex flex-col'>
+                  <h3 className='font-semibold text-foreground/70'>
+                    {group.name}
+                  </h3>
+                  {group.groupTotalSpend && group.groupTotalSpend > 0 ? (
+                    <p className='mt-1 text-xs font-light text-foreground/80'>
+                      {`Total spends: Rs.${group.groupTotalSpend}`}
+                    </p>
+                  ) : (
+                    <p className='mt-1 text-xs font-light text-gray-500'>
+                      No transactions yet
+                    </p>
                   )}
-                </p>
-                <p className='text-sm text-foreground/60'>
-                  {group.LastTransaction?.name}
-                </p>
+                </div>
+                <div className='flex flex-col items-end'>
+                  <p className='text-sm font-light text-foreground/80'>
+                    {group.LastTransaction && (
+                      <DateTimeComponent
+                        dateString={group.LastTransaction?.date}
+                        type='default'
+                      />
+                    )}
+                  </p>
+                  <p className='mt-1 text-sm text-foreground/60'>
+                    {group.LastTransaction?.name}
+                  </p>
+                </div>
               </div>
-            </div>
-          </li>
+            </li>
+            <Divider className='text-foreground-100/40' />
+          </Link>
         ))}
       </ul>
     </article>
--- src/app/(with_nav)/spend-groups/page.tsx
+++ src/app/(with_nav)/spend-groups/page.tsx
@@ -1,44 +1,85 @@
+'use client';
 import { Button } from '@heroui/button';
 import { Divider } from '@heroui/divider';
-import { CirclePlus, Search } from 'lucide-react';
+import { Search } from 'lucide-react';
 import Link from 'next/link';
-import React from 'react';
-import GroupList from './Components/GroupList';
+import React, { Suspense } from 'react';
+import GroupList from './components/GroupList';
+import AddGroupDrawer from './components/AddGroupDrawer';
+import { Tab, Tabs } from '@heroui/tabs';
+import LoadingDots from '@/components/LoadingDots';
 
 export default function SpendGroups() {
   return (
-    <div className='flex flex-col'>
-      <header className='flex flex-row items-center justify-between px-2 py-4'>
-        <span className='pl-2 text-2xl font-semibold text-primary'>
-          Spend Groups
-        </span>
-        <div className='flex gap-4'>
-          <Button
-            isIconOnly
-            radius='full'
-            variant='faded'
-            color='primary'
-            aria-label='search friend'
+    <div className='relative flex flex-col'>
+      <Tabs
+        aria-label='Options'
+        classNames={{
+          tabList:
+            'gap-6 w-full relative rounded-none p-0 border-b border-divider',
+          cursor: 'w-full bg-primary-400',
+          tab: 'px-0 h-12',
+          tabContent:
+            'group-data-[selected=true]:text-primary-400 text-base font-bold',
+        }}
+        color='primary'
+        variant='underlined'
+        fullWidth
+      >
+        <Tab key='created' title='Created'>
+          <header
+            className={`flex flex-row items-center justify-between px-2 pb-2`}
           >
-            <Search />
-            <Link href={'#'}></Link>
-          </Button>
-          <Button
-            isIconOnly
-            radius='full'
-            variant='faded'
-            color='primary'
-            aria-label='Add friend'
-          >
-            <CirclePlus />
-            <Link href={'#'}></Link>
-          </Button>
-        </div>
-      </header>
-      <Divider />
-      <section className='h-[calc(100vh-225px)] overflow-y-auto p-2'>
-        <GroupList />
-      </section>
+            <span className='pl-2 text-2xl font-semibold text-foreground/80'>
+              Created Groups
+            </span>
+            <div className='flex gap-4'>
+              <Button
+                isIconOnly
+                radius='full'
+                variant='faded'
+                color='primary'
+                aria-label='Search Group'
+              >
+                <Search />
+                <Link href={'#'}></Link>
+              </Button>
+              <AddGroupDrawer />
+            </div>
+          </header>
+          <section className='relative h-[calc(100vh-225px)] overflow-y-auto p-2'>
+            <Divider />
+            <Suspense fallback={<LoadingDots />}>
+              <GroupList />
+            </Suspense>
+          </section>
+        </Tab>
+        <Tab key='joined' title='Joined'>
+          <header className='flex flex-row items-center justify-between px-2 pb-2'>
+            <span className='pl-2 text-2xl font-semibold text-foreground/80'>
+              Joined Groups
+            </span>
+            <div className='flex gap-4'>
+              <Button
+                isIconOnly
+                radius='full'
+                variant='faded'
+                color='primary'
+                aria-label='Search Group'
+              >
+                <Search />
+                <Link href={'#'}></Link>
+              </Button>
+            </div>
+          </header>
+          <section className='h-[calc(100vh-225px)] overflow-y-auto p-2'>
+            <Divider />
+            <Suspense fallback={<LoadingDots />}>
+              <GroupList />
+            </Suspense>
+          </section>
+        </Tab>
+      </Tabs>
     </div>
   );
 }
