d93b9a3d6da0ee88198a4359071c9dfa968e6218
ushan senarathna <ushansenarathna000@gmail.com>
2025-04-13 21:31:15 +0530

    feat: add onboarding form to get details of new users

--- src/app/(without_nav)/layout.tsx
+++ src/app/(without_nav)/layout.tsx
@@ -2,14 +2,15 @@ import LoadingDots from '@/components/LoadingDots';
 import { UserInit } from '@/components/UserInit';
 import { Suspense } from 'react';
 
-export default function WithoutNavLayout({
+export default async function WithoutNavLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
     <div>
-      <UserInit /> {/* initialize user data */}
+      {/* Pass server-side user data to client component for hydration */}
+      <UserInit />
       <Suspense fallback={<LoadingDots />}>{children}</Suspense>
     </div>
   );
